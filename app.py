2a4161e9fcb1291fc7cd7c37e3bd78a6657d5fab
Sagufta <saguftasinha19@gmail.com>
2026-08-22 02:28:39 +0530

    Configure MySQL port for deployment

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
 app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
 app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
 app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT", 3306))
 mysql = MySQL(app)
 
 app.secret_key = os.getenv("SECRET_KEY")     # Secret key is required to use Flask sessions
