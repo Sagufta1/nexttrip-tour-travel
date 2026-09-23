b74a68a84cb5d8aec85ed411ee3875d8540b3712
Sagufta <saguftasinha19@gmail.com>
2026-09-23 08:47:49 +0530

    Configure TiDB SSL

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
 app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
 app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
 app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT", 3306))
+app.config["MYSQL_SSL"] = {
+    "ca": "/etc/ssl/certs/ca-certificates.crt"
+}
 mysql = MySQL(app)
 
 app.secret_key = os.getenv("SECRET_KEY")     # Secret key is required to use Flask sessions
